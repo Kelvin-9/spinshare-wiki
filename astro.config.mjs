0eac03e7dd7b0b30fd71025d556b878311424ed2
Dmytro Kloniuk <d.kloniuk@netribegroup.com>
2024-07-24 10:43:46 +0200

    content test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,70 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightUtils from '@lorenzo_lewis/starlight-utils'
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Docs with Tailwind',
+
+			plugins: [starlightUtils({
+				multiSidebar: {
+					switcherStyle: 'dropdown'
+				}
+			})],
+
+			defaultLocale: 'root',
+
+			locales: {
+
+				root: {
+					label: 'English',
+					lang: 'en'
+				},
+				'es': {
+					label: 'Español',
+					lang: 'es'
+				},
+				'de': {
+					label: 'Deutsch',
+					lang: 'de'
+				},
+				'fr': {
+					label: 'Français',
+					lang: 'fr',
+				},
+				'it': {
+					label: 'Italiano',
+					lang: 'it'
+				},
+				'uk': {
+					label: 'Українська',
+					lang: 'uk'
+				},
+				'ru': {
+					label: 'Русский',
+					lang: 'ru'
+				},
+				'ja': {
+					label: '日本語',
+					lang: 'ja',
+				},
+				'ko': {
+					label: '한국어',
+					lang: 'ko'
+				},
+				'zh-cn': {
+					label: '简体中文',
+					lang: 'zh-CN'
+				},
+				'zh-tw': {
+					label: '正體中文',
+					lang: 'zh-TW'
+				}
+			},
+
+			title: 'SpinShare Wiki',
 			social: {
 				github: 'https://github.com/withastro/starlight',
 			},
